8a91d7f3714376bb0d51d2aa3fd21a319927a7d8
Irwingduran <86335744+Irwingduran@users.noreply.github.com>
2025-03-26 20:36:05 -0600

    Update page.tsx

--- app/en/service2/page.tsx
+++ app/en/service2/page.tsx
@@ -146,7 +146,7 @@ export default function GastricSleevePage() {
             <p className="text-gray-600 mb-8">
               This is your moment to take control and improve your health. Schedule a consultation and discover if Gastric Sleeve is the right option for you.
             </p>
-            <Link href="https://wa.me/5212221843622?text=Hola%20Dr.%20encontr%C3%A9%20su%20perfil%20en%20findoctor%20y%20me%20gustar%C3%ADa%20realizar%20una%20consulta">
+            <Link href="https://wa.me/5212221843622?text=Hola%20Dr.%20encontr%C3%A9%20su%20sitio%20web%20y%20me%20gustar%C3%ADa%20realizar%20una%20consulta">
               <Button className="bg-[#FFB800] text-white hover:bg-[#0056A3] px-8 py-4">
                 Schedule Your Evaluation
               </Button>
