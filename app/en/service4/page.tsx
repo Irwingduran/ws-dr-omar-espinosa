3e48f10d958f6e0f8fd6ed504e6411dd830d0932
arobles02 <arobles02@hotmail.com>
2025-07-08 09:36:07 -0600

    Update page.tsx

--- app/en/service4/page.tsx
+++ app/en/service4/page.tsx
@@ -41,7 +41,12 @@ export default function HerniaInguinalPage() {
             <h1 className="text-3xl md:text-5xl font-light mb-4">Inguinal Hernia</h1>
             <p className="text-lg">
               An effective and safe solution to correct abdominal wall defects.
-            </p>
+            </p><br></br>
+            <Link href="https://wa.me/5212221843622?text=Hello%20Doctor%2C%20I%20found%20your%20website%20and%20I%20would%20like%20to%20make%20an%20inquiry">
+              <Button className="bg-[#FFB800] text-white hover:bg-[#0056A3] px-6 py-3 md:px-8 md:py-4">
+                Schedule appointment
+              </Button>
+            </Link>
           </div>
         </div>
 
